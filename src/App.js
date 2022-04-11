ad71289b7a7f0e20677d42097df6202d318cdf50
jonatasnb713 <jonatasnb713@hotmail.com>
2022-04-10 22:57:09 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Portal Controle De Documentos </p>
+        <p>Portal Controle De Documentos</p>
         
        
       </header>
