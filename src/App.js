6472b5abe7fb48416ff619c53f1362eed6f53991
jonatasnb713 <jonatasnb713@hotmail.com>
2022-05-18 01:26:16 -0300

    fazendo testes com a API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [CLientList, setCLientList] = useState({});
+
+  useEffect  (() => {
+    console.log("pedindo")
+     fetch('http://localhost:5000/clients')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        const list = {...data}
+        setCLientList(list)
+      }).catch((err) => {console.error(err)})
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Portal Controle De Documentos</p>
-        
-       
+        <h2>
+        {JSON.stringify(CLientList)}
+        </h2>
       </header>
     </div>
   );
 }
-
 export default App;
